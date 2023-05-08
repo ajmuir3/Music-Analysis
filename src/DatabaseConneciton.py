9ec0d1bb962b559c31bc54371c97e31cf9afc508
AJ Muir <andrew.muir.22@cnu.edu>
2023-05-08 01:01:32 -0400

    last commit of the night

--- src/DatabaseConneciton.py
+++ src/DatabaseConneciton.py
@@ -1,12 +1,25 @@
 import sqlite3
 import os
-
 from Song import Song
 
+def readCSV(filename):
+    song_list = []
+    errors = []
+    with open(filename,"r") as file:
+        for line in file:
+            try:
+                entry = line.split(",")
+                song_list.append(Song(entry[0], entry[1], entry[2], int(entry[3]), int(entry[4].split("\n")[0])))
+            except ValueError as e:
+                errors.append("{entry} cannot be added due to {error}".format(entry = line, error = e))
+        # for entry in errors:
+        #     print(entry)
+    return song_list
+
 # Define the function to insert data from a list of Song objects
-def insert_data(songs):
+def insert_data(database,songs):
     # Connect to the database
-    conn = sqlite3.connect('music_database.db')
+    conn = sqlite3.connect(database)
 
     # Create a cursor object
     c = conn.cursor()
@@ -18,12 +31,11 @@ def insert_data(songs):
                  artist TEXT,
                  genre TEXT,
                  plays INTEGER,
-                 date_added TEXT,
                  year_released INTEGER)''')
 
     # Insert the data from the list of Song objects
     for song in songs:
-        c.execute("INSERT INTO music_data (song_name, artist, genre, plays, date_added, year_released) VALUES (?, ?, ?, ?, ?, ?)", (song.name, song.artist, song.genre, song.plays, song.date_added, song.year_released))
+        c.execute("INSERT INTO music_data (song_name, artist, genre, plays, year_released) VALUES (?, ?, ?, ?, ?)", (song.name, song.artist, song.genre, song.plays, song.year_released))
 
     # Commit the changes to the database
     conn.commit()
@@ -32,9 +44,9 @@ def insert_data(songs):
     conn.close()
 
 # Define the function to display the data
-def display_data():
+def display_data(database):
     # Connect to the database
-    conn = sqlite3.connect('music_database.db')
+    conn = sqlite3.connect(database)
 
     # Create a cursor object
     c = conn.cursor()
@@ -50,27 +62,52 @@ def display_data():
     # Close the connection
     conn.close()
 
-def clear_data():
+def return_data(database):
+    # Connect to the database
+    conn = sqlite3.connect(database)
+
+    # Create a cursor object
+    c = conn.cursor()
+
+    # Select all the data from the table
+    c.execute("SELECT * FROM music_data")
+
+    # Fetch and display the data
+    data = []
+    for entry in c.fetchall():
+        data.append(entry)
+    
+    # Close the connection
+    conn.close()
+
+    # Return Data
+    return data
+
+def clear_data(database):
     # Remove the database file if it exists
-    if os.path.exists('music_database.db'):
-        os.remove('music_database.db')
+    if os.path.exists(database):
+        os.remove(database)
         print("Database dropped successfully.")
     else:
         print("Database does not exist.")
 
-# Create a list of Song objects
-songs = [
-    Song('Song 1', 'Artist 1', 'Genre1', 20, 'Today', 1990)
-    # Add more songs here
-]
+# def main():
+#     # Create a list of Song objects
+#     songs = [
+#         Song('Song 1', 'Artist 1', 'Genre1', 20, 'Today', 1990)
+#         # Add more songs here
+#     ]
 
-# Insert the data into the database
-insert_data(songs)
+#     database = "music_database.db"
 
-# Display the data from the database
-display_data()
+#     # Insert the data into the database
+#     insert_data(database, songs)
 
-# Clear the data from the datadabe
-clear_data()
+#     # Display the data from the database
+#     display_data(database)
 
+#     # Clear the data from the datadabe
+#     clear_data(database)
+
+# main()
 
--- src/Song.py
+++ src/Song.py
@@ -1,11 +1,13 @@
 class Song:
-    def __init__(self, n, a, g, p, dA, yR):
+    def __init__(self, n, a, g, p, yR):
         self.name = n
         self.artist = a
         self.genre = g
         self.plays = p
-        self.date_added = dA
         self.year_released = yR
+
+    def __lt__(self,other):
+        return self.year<other.year
 
     def __str__(self):
         return f"{self.name}, {self.artist} ({self.year_released})"
